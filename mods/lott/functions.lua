0394a5e2c547e1a8c7558fca06285e40b8429db2
Doloment <sawerter@inbox.ru>
2019-10-31 00:02:18 +0700

    Add client-side translation func

--- mods/lott/functions.lua
+++ mods/lott/functions.lua
@@ -5,6 +5,10 @@ function lott.str_to_desc(str)
 	return string.gsub(" " .. str:gsub("_", " "), "%W%l", string.upper):sub(2)
 end
 
+function lott.s(str)
+	minetest.get_translator(minetest.get_current_modname())
+end
+
 function lott.swap_node(pos, name)
 	local node = minetest.get_node(pos)
 	if node.name == name then
